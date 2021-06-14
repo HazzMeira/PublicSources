6a634ff9bd1c799c18c7c4b655bb16a5a865b549
HazzMeira <hmeira@outlook.com>
2021-06-14 16:56:03 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
+
+const routes = Router();
+
+//Métodos HTTP: get, post, put, delete
+//Query params: req.query (filters, orders, paginations)
+//Route params: req.params (id)
+//Body params: req.body (create obj)
+
+//MongoDB (Não-relacional)
+
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+
+routes.get('/search', SearchController.index);
+
+module.exports = routes;
